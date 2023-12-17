6592d87f9a38bae87cb26a46d3ecccc27c0b5316
Diana Coelho <diana@MacBook-Pro-de-Diana.local>
2023-12-17 16:04:28 -0300

    entendendo nx

---
+++ angular-monorepo/apps/playing-w-forms/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { SharedUiComponent } from '@angular-monorepo/shared-ui';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, SharedUiComponent],
+  selector: 'angular-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'playing-w-forms';
+}
